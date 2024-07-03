a2786f221d513b017856fe1d0633fa5135dfcd9c
tolnabert <tolnabert@gmail.com>
2024-07-03 17:44:05 +0200

    add docker multistage

---
+++ express/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+const app = express(); 
+
+app.use('/api', createProxyMiddleware({
+  target: 'http://backend:8080/api', //just dev
+  changeOrigin: true
+}))
+
+app.use(express.urlencoded({ extended: true })) 
+app.use(express.json()) 
+app.use(express.static('public'))
+
+app.listen(5000, 
+() => console.log(`[bootup]: Server is running at port: 5000`));
