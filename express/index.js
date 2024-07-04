078658db6c27a99dad997fb19442d976562d237f
tolnabert <tolnabert@gmail.com>
2024-07-04 14:21:20 +0200

    feat dockerization, after react npm build and dist fodler copied to express can run docker-compose up

--- express/index.js
+++ express/index.js
@@ -10,7 +10,7 @@ app.use('/api', createProxyMiddleware({
 
 app.use(express.urlencoded({ extended: true })) 
 app.use(express.json()) 
-app.use(express.static('public'))
+app.use(express.static('dist'))
 
 app.listen(5000, 
 () => console.log(`[bootup]: Server is running at port: 5000`));
